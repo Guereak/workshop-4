40ca93517e2ccf9add9f1a1daaf075ad6e5902c8
Maxime L. <66002207+Guereak@users.noreply.github.com>
2025-02-25 17:53:43 +0100

    1 and 2

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,14 +1,37 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
+import { GetNodeRegistryBody,  } from "../registry/registry";
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
-  // TODO implement the status route
-  // onionRouter.get("/status", (req, res) => {});
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: number | null = null;
+
+  onionRouter.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.send({result: lastReceivedEncryptedMessage})
+  });
+
+  onionRouter.get("/getLastRecievedDecryptedMessage", (req, res) => {
+    res.send({result: lastReceivedDecryptedMessage})
+  })
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.send({result: lastMessageDestination})
+  })
+
+  onionRouter.get("/registerNode", (req, res) => {
+    res.send({})
+  })
+
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
--- src/users/user.ts
+++ src/users/user.ts
@@ -12,8 +12,20 @@ export async function user(userId: number) {
   _user.use(express.json());
   _user.use(bodyParser.json());
 
-  // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
+  let lastReceivedMessage: string | null = null;
+  let lastSentMessage: string | null = null;
+
+  _user.get("/status", (req, res) => {
+    res.send("live")
+  });
+
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.send({result: lastReceivedMessage})
+  });
+
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.send({result: lastSentMessage})
+  });
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
